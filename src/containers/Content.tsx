0a2324c6b0d488a9eb8147d639ab9fcbb2fd8577
peterkwkwan <peterkwkwan@gmail.com>
2022-05-23 21:52:11 +0800

    refactor: routerConfig location

--- src/containers/Content.tsx
+++ src/containers/Content.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { routerConfig } from "../constants/routerConfig";
+import { routerConfig } from "../shared/routerConfig";
 
 export const Content = () => {
   return (
