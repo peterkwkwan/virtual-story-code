6085c0538b3edc61c0696cd0e05e77c40d77795f
Peter Kwan <peterkwkwan@gmail.com>
2022-05-30 01:34:11 +0800

    styled text

---
+++ src/components/explorer-pages/shared/StyledText.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const ImportExportText = styled.span`
+  color: ${(props) => props.theme.color.importExportText};
+`;
+
+export const StringText = styled.span`
+  color: ${(props) => props.theme.color.stringText};
+`;
+
+export const PropertyText = styled.span`
+  color: ${(props) => props.theme.color.propertyText};
+`;
+
+export const DeclarationText = styled.span`
+  color: ${(props) => props.theme.color.declarationText};
+`;
+
+export const VariableText = styled.span`
+  color: ${(props) => props.theme.color.variableText};
+`;
