ace7fdd909380db401256ff5979f22f2fa4a05f6
Peter Kwan <peterkwkwan@gmail.com>
2022-05-31 23:30:42 +0800

    refactor: remove useEffect

--- src/containers/SidebarBtns.tsx
+++ src/containers/SidebarBtns.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { StyledLink } from "../components/StyledLink";
