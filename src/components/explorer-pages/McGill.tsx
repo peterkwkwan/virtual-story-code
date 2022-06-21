39c05a56b3e51a4c43b2b094aa470c234d5db769
Peter Kwan <peterkwkwan@gmail.com>
2022-06-21 13:53:22 +0800

    feat: Education

--- src/components/explorer-pages/McGill.tsx
+++ src/components/explorer-pages/McGill.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { useLastContributed } from "../../utils/useLastContributed";
+import { EducationDescriptionHTML } from "./shared/EducationDescriptionHTML";
+import { ExplorerWrapper } from "./shared/ExplorerWrapper";
 
 export const McGill = () => {
-  return <div>McGill</div>;
+  const date = new Date("2014-05-01");
+  const diff = useLastContributed(date);
+  const contributors = `${diff} | 3 authors (Mom & Dad and 1 other)`;
+
+  return (
+    <ExplorerWrapper contributors={contributors} numberOfLines={20}>
+      <EducationDescriptionHTML />
+    </ExplorerWrapper>
+  );
 };
