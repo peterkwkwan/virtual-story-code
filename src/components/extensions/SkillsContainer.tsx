a485ba89cda309a5fb563b4a7cd22492a9a345d5
Peter Kwan <peterkwkwan@gmail.com>
2022-06-24 22:26:22 +0800

    feat: matrix background

--- src/components/extensions/SkillsContainer.tsx
+++ src/components/extensions/SkillsContainer.tsx
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
+interface StyledFolder {
+  opened: boolean;
+}
+
+const Container = styled.div`
+  margin: 12px 0 0;
+`;
+
+const FolderButton = styled.button<StyledFolder>`
+  width: 100%;
+  background-color: inherit;
+  border: none;
+  line-height: 22px;
+  padding: 0;
+  white-space: pre;
+  text-decoration: none;
+  font-size: 11px;
+  font-weight: 600;
+  font-family: sans-serif;
+  text-align: start;
+  color: inherit;
+  cursor: pointer;
+  &:before {
+    content: url("../../assets/icons/arrow.svg");
+    display: inline-block;
+    transform: ${(props) => props.opened && "rotate(90deg)"};
+    margin: 0 5px;
+  }
+
+  &:hover {
+    background-color: ${(props) => props.theme.color.buttonFocus};
+  }
+`;
+
+const techStack = [
+  { title: "React", img: "" },
+  { title: "JavaScript", img: "" },
+  { title: "TypeScript", img: "" },
+];
 
 export const SkillsContainer = () => {
-  return <div>SkillsContainer</div>;
+  const [opened, setOpened] = useState(true);
+  const handleFolderClick = () => {
+    setOpened((prevState) => !prevState);
+  };
+
+  return (
+    <Container>
+      <FolderButton opened={opened} onClick={handleFolderClick}>
+        INSTALLED
+      </FolderButton>
+      {opened &&
+        techStack.map((tech) => <div key={tech.title}>{tech.title}</div>)}
+    </Container>
+  );
 };
--- src/components/search/ContentContainer.tsx
+++ src/components/search/ContentContainer.tsx
@@ -9,6 +9,9 @@ const Container = styled.div`
   height: 100%;
   grid-template-columns: repeat(2, 1fr);
   grid-template-rows: repeat(2, 1fr);
+  background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
+    url(https://i.pinimg.com/originals/30/09/e7/3009e722ec057ffab1e41d2f993385d1.png);
+  background-size: cover;
 `;
 
 const Profiles = [
