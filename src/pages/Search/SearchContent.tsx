1a8dfedade2514f11663c844127d82efd8caefe0
peterkwkwan <peterkwkwan@gmail.com>
2022-08-07 13:54:03 +0800

    feat: eslinting

--- src/pages/Search/SearchContent.tsx
+++ src/pages/Search/SearchContent.tsx
@@ -1,11 +1,12 @@
-import React from "react";
-import { BaseContentContainer } from "../shared/styledContainers";
-import { ContentContainer } from "./ContentContainer";
+import React from 'react'
+
+import { BaseContentContainer } from '../shared/styledContainers'
+import { ContentContainer } from './ContentContainer'
 
 export const SearchContent = () => {
   return (
     <BaseContentContainer>
       <ContentContainer />
     </BaseContentContainer>
-  );
-};
+  )
+}
