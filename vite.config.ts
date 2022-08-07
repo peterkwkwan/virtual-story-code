970b30dcc0ed31e3ba8760b173489ca9bf70acb1
Peter Kwan <peter@bcw.group>
2022-08-07 13:36:13 +0800

    feat: eslinting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import path from 'path'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,4 +10,4 @@ export default defineConfig({
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-});
+})
