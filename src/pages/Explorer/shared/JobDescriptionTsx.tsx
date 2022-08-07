2cfc79c557edd866c9fdb40ec6d031b994221662
peterkwkwan <peterkwkwan@gmail.com>
2022-08-07 14:06:09 +0800

    feat: eslinting

--- src/pages/Explorer/shared/JobDescriptionTsx.tsx
+++ src/pages/Explorer/shared/JobDescriptionTsx.tsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { ReactImports } from "./ReactImports";
+import React from 'react'
+
+import { ReactImports } from './ReactImports'
 import {
   DarkBlueText,
   PurpleText,
@@ -11,8 +12,8 @@ import {
   GreenText,
   LightYellowText,
   YellowBrackets,
-} from "./StyledText";
-import { IJobDescription } from "./types";
+} from './StyledText'
+import { IJobDescription } from './types'
 
 export const JobDescriptionTsx = ({
   jobDescription,
@@ -28,51 +29,51 @@ export const JobDescriptionTsx = ({
     companyDescription,
     functions,
     techStack,
-  } = jobDescription;
+  } = jobDescription
 
   const basicInfo: Partial<IJobDescription> = {
     date: date,
     role: role,
     location: location,
     companyDescription: companyDescription,
-  };
+  }
 
   /* eslint-disable react/no-unescaped-entities */
 
   const renderBasicInfo = () => {
-    const elementList: JSX.Element[] = [];
+    const elementList: JSX.Element[] = []
     for (const property in basicInfo) {
       elementList.push(
         <div key={property}>
-          <DarkBlueText>const</DarkBlueText> <BlueText>{property}</BlueText> ={" "}
+          <DarkBlueText>const</DarkBlueText> <BlueText>{property}</BlueText> ={' '}
           <StringText>
             "{basicInfo[property as keyof Partial<IJobDescription>]}"
           </StringText>
         </div>
-      );
+      )
     }
-    return elementList;
-  };
+    return elementList
+  }
 
   return (
     <>
       <ReactImports />
       <div>
-        <PurpleText>export</PurpleText> <DarkBlueText>const</DarkBlueText>{" "}
-        <LightYellowText>{companyName}</LightYellowText> ={" "}
-        <YellowBrackets>{"() "}</YellowBrackets>
-        {"=> "} <YellowBrackets>{"{"}</YellowBrackets>
+        <PurpleText>export</PurpleText> <DarkBlueText>const</DarkBlueText>{' '}
+        <LightYellowText>{companyName}</LightYellowText> ={' '}
+        <YellowBrackets>{'() '}</YellowBrackets>
+        {'=> '} <YellowBrackets>{'{'}</YellowBrackets>
         <Indent>
           {isCurrentRole && (
             <>
               {/* <Indent> */}
-              <DarkBlueText>const</DarkBlueText> <PurpleText>{"["}</PurpleText>
+              <DarkBlueText>const</DarkBlueText> <PurpleText>[</PurpleText>
               <BlueText>isCurrentRole</BlueText>
-              <PurpleText>{"]"}</PurpleText> = <GreenText>React</GreenText>.
+              <PurpleText>]</PurpleText> = <GreenText>React</GreenText>.
               <LightYellowText>useState</LightYellowText>
-              <PurpleText>{"("}</PurpleText>
+              <PurpleText>(</PurpleText>
               <BlueText>true</BlueText>
-              <PurpleText>{")"}</PurpleText>
+              <PurpleText>)</PurpleText>
               {/* </Indent> */}
             </>
           )}
@@ -81,9 +82,9 @@ export const JobDescriptionTsx = ({
           <DarkBlueText>const </DarkBlueText>
           <BlueText>job </BlueText>= <DarkBlueText>new </DarkBlueText>
           <GreenText>Job</GreenText>
-          <PurpleText>{"("}</PurpleText>
+          <PurpleText>(</PurpleText>
           <StringText>"{companyName}"</StringText>
-          <PurpleText>{")"}</PurpleText>
+          <PurpleText>)</PurpleText>
           <LineBreak />
           {/* </Indent> */}
           {renderBasicInfo()}
@@ -91,40 +92,40 @@ export const JobDescriptionTsx = ({
             return (
               <React.Fragment key={task}>
                 <LineBreak />
-                <DarkBlueText>const</DarkBlueText>{" "}
-                <LightYellowText>project{idx + 1}</LightYellowText> ={" "}
-                <DarkBlueText>function</DarkBlueText>{" "}
-                <PurpleText>{"() {"} </PurpleText>
+                <DarkBlueText>const</DarkBlueText>{' '}
+                <LightYellowText>project{idx + 1}</LightYellowText> ={' '}
+                <DarkBlueText>function</DarkBlueText>{' '}
+                <PurpleText>{'() {'} </PurpleText>
                 <Indent>
                   <LightBlueText>console</LightBlueText>.
                   <LightYellowText>log</LightYellowText>
-                  <DarkBlueText>{"("}</DarkBlueText>
+                  <DarkBlueText>(</DarkBlueText>
                   <StringText>"{task}"</StringText>
-                  <DarkBlueText>{")"}</DarkBlueText>
+                  <DarkBlueText>)</DarkBlueText>
                 </Indent>
-                <PurpleText>{"}"} </PurpleText>
+                <PurpleText>{'}'} </PurpleText>
               </React.Fragment>
-            );
+            )
           })}
           <LineBreak />
           <>
-            <DarkBlueText>const</DarkBlueText> <BlueText>techStack</BlueText> ={" "}
-            <PurpleText>{"["} </PurpleText>
+            <DarkBlueText>const</DarkBlueText> <BlueText>techStack</BlueText> ={' '}
+            <PurpleText>[ </PurpleText>
             <Indent>
               {techStack?.map((stack) => {
                 return (
                   <div key={stack}>
                     <StringText>"{stack}",</StringText>
                   </div>
-                );
+                )
               })}
             </Indent>
-            <PurpleText>{"]"} </PurpleText>
+            <PurpleText>] </PurpleText>
           </>
         </Indent>
-        <YellowBrackets>{"}"}</YellowBrackets>
+        <YellowBrackets>{'}'}</YellowBrackets>
       </div>
     </>
-  );
-};
+  )
+}
 /* eslint-enable react/no-unescaped-entities */
