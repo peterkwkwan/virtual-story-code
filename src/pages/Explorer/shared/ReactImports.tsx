073bed93bd1632813b0459090e76cc8683f593b5
peterkwkwan <peterkwkwan@gmail.com>
2022-08-07 14:06:15 +0800

    feat: eslinting

--- src/pages/Explorer/shared/ReactImports.tsx
+++ src/pages/Explorer/shared/ReactImports.tsx
@@ -1,31 +1,32 @@
-import React from "react";
-import styled from "styled-components";
-import TypewriterComponent from "typewriter-effect";
-import { LightBlueText, LineBreak, PurpleText, StringText } from "./StyledText";
+import React from 'react'
+import styled from 'styled-components'
+import TypewriterComponent from 'typewriter-effect'
+
+import { LightBlueText, LineBreak, PurpleText, StringText } from './StyledText'
 
 const StyledImport = styled.div`
   margin: 0;
   font-family: Menlo, Monaco, "Courier New", monospace;
-`;
+`
 
 /* eslint-disable react/no-unescaped-entities */
 export const ReactImports = () => {
-  const typewriterString = `<span style="color: #c586c0;">import</span> <span style="color: #9cdcfe;">Job</span> <span style="color: #c586c0;">from</span> <span style="color: #ce9178;">'career'</span>`;
+  const typewriterString = '<span style="color: #c586c0;">import</span> <span style="color: #9cdcfe;">Job</span> <span style="color: #c586c0;">from</span> <span style="color: #ce9178;">\'career\'</span>'
   return (
     <>
       <StyledImport>
-        <PurpleText>import</PurpleText> <LightBlueText>React</LightBlueText>{" "}
+        <PurpleText>import</PurpleText> <LightBlueText>React</LightBlueText>{' '}
         <PurpleText>from</PurpleText> <StringText>'react'</StringText>
-        <div style={{ height: "18px" }}>
+        <div style={{ height: '18px' }}>
           <TypewriterComponent
             onInit={(typewriter) => {
-              typewriter.typeString(typewriterString).pauseFor(2500).start();
+              typewriter.typeString(typewriterString).pauseFor(2500).start()
             }}
           />
         </div>
       </StyledImport>
       <LineBreak />
     </>
-  );
-};
+  )
+}
 /* eslint-enable react/no-unescaped-entities */
