1e904f0b19edce86b2ec3b5673be150f985f629e
peterkwkwan <peterkwkwan@gmail.com>
2022-10-09 00:22:17 +0800

    feat: cross cancel to route tabs

--- src/components/ui/RouteTab.tsx
+++ src/components/ui/RouteTab.tsx
@@ -6,20 +6,39 @@ import { PageNames } from '@/pages/shared/routerConfig'
 const Container = styled.div`
   height: 38px;
   width: 100%;
-  color: white;
-  font-family: 'Segoe UI', Roboto, 'Open Sans', 'Helvetica Neue', sans-serif;
-  font-size: 13px;
+  display: flex;
   line-height: 35px;
+  color: white;
+`
+
+const Label = styled.p`
+  font-family: "Segoe UI", Roboto, "Open Sans", "Helvetica Neue", sans-serif;
+  font-size: 13px;
+  margin: 0;
   text-overflow: ellipsis;
-  text-decoration: none;
+`
+
+const Cross = styled.div`
+  margin-left: 4px;
+  padding: 2px;
+  ::before {
+    content: "\\00d7";
+  }
+  :hover{
+    background: ${props => props.theme.palette.dark02};
+    cursor: pointer;
+  }
 `
 
 interface Props {
-  name: PageNames
+  name: PageNames;
 }
 
-export const RouteTab = ({name} : Props) => {
+export const RouteTab = ({ name }: Props) => {
   return (
-    <Container>{name}</Container>
+    <Container>
+      <Label>{name}</Label>
+      <Cross />
+    </Container>
   )
 }
