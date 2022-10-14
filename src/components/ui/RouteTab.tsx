4a59c2207ba7c451ca96c8534a7921729dc4a3d5
peterkwkwan <peterkwkwan@gmail.com>
2022-10-15 00:49:31 +0800

    feat: tab styling

--- src/components/ui/RouteTab.tsx
+++ src/components/ui/RouteTab.tsx
@@ -6,9 +6,18 @@ import { PageNames } from '@/pages/shared/routerConfig'
 const Container = styled.div`
   height: 38px;
   width: 100%;
+  background-color: ${props => props.theme.palette.dark03};
+`
+
+const Tab = styled.div`
+  height: 100%;
+  width: fit-content;
+  padding: 0 4px 0 10px;
   display: flex;
+  align-items: center;
   line-height: 35px;
   color: white;
+  background-color: ${props => props.theme.palette.dark01};
 `
 
 const Label = styled.p`
@@ -21,13 +30,21 @@ const Label = styled.p`
 const Cross = styled.div`
   margin-left: 4px;
   padding: 2px;
-  ::before {
-    content: "\\00d7";
-  }
+  height: 18px;
+  width: 18px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   :hover{
     background: ${props => props.theme.palette.dark02};
     cursor: pointer;
+    
   }
+  ::after{
+    font-size: 18px;
+    font-weight: 200;
+      content: "\\00d7";
+    }
 `
 
 interface Props {
@@ -37,8 +54,10 @@ interface Props {
 export const RouteTab = ({ name }: Props) => {
   return (
     <Container>
-      <Label>{name}</Label>
-      <Cross />
+      <Tab>
+        <Label>{name}</Label>
+        <Cross/>
+      </Tab>
     </Container>
   )
 }
