cb70f692a271a7d3ec410db89acf388cd281890b
Peter Kwan <37263010+peterkwkwan@users.noreply.github.com>
2022-10-26 13:41:58 +0800

    Update Description.tsx

--- src/pages/Explorer/Home/ParallaxLayers/Description.tsx
+++ src/pages/Explorer/Home/ParallaxLayers/Description.tsx
@@ -1,25 +1,58 @@
+
 import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
 margin-left: 5%;
 margin-top: 5%;
-  border-radius: 15rem;
+  border-radius: 30rem;
   background-color: rgba(0, 0, 0, 0.5);
-  height: 50%;
+  height: 35%;
   width: 80%;
   display: flex;
   justify-content: center;
   align-items: center;
+  &:before {
+    content: 'Peter';
+    font-size: 2rem;
+    margin-right: 4px;
+    position: absolute;
+    top: 6%;
+    left: 18%;
+    color: ${(props) => props.theme.palette.white};
+  }
 `
 
 const InnerBorder = styled.div`
   height: 85%;
-  width: 95%;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
+  &:before {
+    content: '';
+    background: url("../../assets/images/speech-bubble-border.png");
+    background-position: center;
+    height: 50%;
+    width: 5%;
+    background-size: contain;
+    background-repeat: no-repeat;
+    position: relative;
+    left: -2%;
+  }
+  &:after {
+    content: '';
+    background: url("../../assets/images/speech-bubble-border.png");
+    background-position: center;
+    transform: rotate(180deg);
+    height: 50%;
+    width: 5%;
+    background-size: contain;
+    background-repeat: no-repeat;
+    position: relative;
+    right: -2%;
+  }
+`
 
 const DescriptionText = styled.h6`
   color: ${(props) => props.theme.palette.white};
@@ -36,3 +69,4 @@ export const Description = () => {
     </Container>
   )
 }
+
