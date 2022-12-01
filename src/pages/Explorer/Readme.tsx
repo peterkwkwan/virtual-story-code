0d7753fe6fe44fe34eef62f3a0d2b9582208487d
peterkwkwan <peterkwkwan@gmail.com>
2022-12-02 00:36:42 +0800

    feat: fix line spacing and height

--- src/pages/Explorer/Readme.tsx
+++ src/pages/Explorer/Readme.tsx
@@ -6,30 +6,40 @@ import { BaseText, DarkBlueText, LineBreak, StringText } from './shared/StyledTe
 
 import { useLastContributed } from '@/hooks/useLastContributed'
 
+const BlockBaseText = styled(BaseText)`
+  display: block;
+`
+
 export const Readme = () => {
   const date = new Date('2019-05-01')
   const diff = useLastContributed(date)
   const contributors = `${diff} | 2 authors (Mandy Shum and 1 other)`
 
   return (
-    <ExplorerWrapper contributors={contributors} numberOfLines={12}>
+    <ExplorerWrapper contributors={contributors} numberOfLines={13}>
       <DarkBlueText>## virtual story code</DarkBlueText>
       <LineBreak/>
+      <BlockBaseText>
       this is my virtual story
+      </BlockBaseText>
       <LineBreak/>
+      <BlockBaseText>
       journey started 2017 December
+      </BlockBaseText>
       <LineBreak/>
+      <BlockBaseText>
       Project tech stack:
+      </BlockBaseText>
       <LineBreak/>
-      <BaseText>
+      <BlockBaseText>
         <DarkBlueText>1. </DarkBlueText>[<StringText>Vite</StringText>](https://vitejs.dev/) - for Über fast local hosting & bundling speeds
-      </BaseText>
-      <div>
+      </BlockBaseText>
+      <BlockBaseText>
         <DarkBlueText>2. </DarkBlueText>[<StringText>styled-components</StringText>](https://styled-components.com/) - CSS in JS, global theming, and conditional styling ;&#41;
-      </div>
-      <div>
+      </BlockBaseText>
+      <BlockBaseText>
         <DarkBlueText>3. </DarkBlueText>[<StringText>React Router</StringText>](https://reactrouter.com/) - native-like site navigation
-      </div>
+      </BlockBaseText>
     </ExplorerWrapper>
   )
 }
