5c2ee9c0781a44acee6dea9a40f0eb4a2967a0fd
peterkwkwan <peterkwkwan@gmail.com>
2022-12-03 10:46:14 +0800

    feat: cropped corner borders

--- src/pages/Explorer/Home/ParallaxLayers/MyCareer/Title.tsx
+++ src/pages/Explorer/Home/ParallaxLayers/MyCareer/Title.tsx
@@ -9,18 +9,42 @@ const Container = styled.div`
   font-family: "League Spartan";
   color: ${(props) => props.theme.palette.white};
   text-transform: uppercase;
+  position: relative;
+  padding: 20px;
+  border: 5px solid ${(props) => props.theme.palette.white};
+  &::before, &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    background: ${(props) => props.theme.palette.darkOrange};
+  }
+  &::before {
+    top: -1rem;
+    bottom: -1rem;
+    left: 1rem;
+    right: 1rem;
+  }
+  
+  &::after{
+    left: -1rem;
+    right: -1rem;
+    top: 1rem;
+    bottom: 1rem;
+  }
 `
 
 const Header = styled.h3`
   font-weight: 400;
   font-size: 7rem;
   margin: 0;
-`
+  z-index: 10;
+  `
 
 const Subtitle = styled.h6`
   font-weight: 200;
   font-size: 2rem;
   margin: 0;
+  z-index: 10;
 `
 
 export const Title = () => {
