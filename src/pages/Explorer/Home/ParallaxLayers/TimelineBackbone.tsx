5aa6f0e39826cf68817af847b5cbe29d93ac0691
peterkwkwan <peterkwkwan@gmail.com>
2023-02-17 21:55:57 +0800

    feat: mario vine

--- src/pages/Explorer/Home/ParallaxLayers/TimelineBackbone.tsx
+++ src/pages/Explorer/Home/ParallaxLayers/TimelineBackbone.tsx
@@ -2,21 +2,30 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100%;
-    width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
 `
 
 const Backbone = styled.div`
-    width: 12px;
-    height: 100%;
-    background: ${props => props.theme.palette.dark01};
-
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
+
+const MarioVine = styled.img`
+  height: 100%;
+`
+
 export const TimelineBackbone = () => {
   return (
-    <Container><Backbone/></Container>
+    <Container>
+      <Backbone>
+        <MarioVine src="/assets/images/mario-vine.svg" alt="mario-vine" />
+      </Backbone>
+    </Container>
   )
 }
