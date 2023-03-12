b3f076d78b787d78fead58c05def99f33d3433d6
peterkwkwan <peterkwkwan@gmail.com>
2023-03-13 00:09:57 +0800

    feat: update styling intro

--- src/pages/Explorer/Home/ParallaxLayers/Intro/Intro.tsx
+++ src/pages/Explorer/Home/ParallaxLayers/Intro/Intro.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { ScrollPrompt } from '../shared/ScrollPrompt'
+import { IDENTITY } from '../shared/constants'
+
+import { useStore } from '@/hooks/useStore'
 
 const Container = styled.div`
   height: 100%;
@@ -12,22 +15,26 @@ const Container = styled.div`
   flex-direction: column;
   position: relative;
 `
-const NameContainer = styled.div`
+const NameContainer = styled.div<{ isVisible: boolean }>`
   position: relative;
   display: flex;
+  text-shadow: ${(props) => (props.isVisible ? '1px 1px 2px #000' : '')};
+  transition: text-shadow 1s;
+
   &::before,
   &::after {
     content: '';
     display: block;
     position: absolute;
+    transition: border 0.5s;
   }
   &::before {
     top: -1rem;
     left: -0.5rem;
     height: 80%;
     width: 5%;
-    border-top: 5px solid ${(props) => props.theme.palette.darkOrange};
-    border-left: 5px solid ${(props) => props.theme.palette.darkOrange};
+    border-top: 5px solid white;
+    border-left: 5px solid white;
   }
 
   &::after {
@@ -35,64 +42,92 @@ const NameContainer = styled.div`
     right: -1rem;
     height: 80%;
     width: 5%;
-    border-bottom: 5px solid ${(props) => props.theme.palette.white};
-    border-right: 5px solid ${(props) => props.theme.palette.white};
+    border-bottom: 5px solid white;
+    border-right: 5px solid white;
   }
 `
 
-const HeaderText = styled.div`
+const HeaderText = styled.div<{ isVisible: boolean }>`
   position: relative;
+  margin: 0;
+  font-family: 'League Spartan';
+  text-transform: uppercase;
+  transition: all 1s;
 `
 
-const Hello = styled(HeaderText)`
-  writing-mode: vertical-rl;
-  text-transform: uppercase;
-  font-size: 6rem;
+const HelloThere = styled(HeaderText)`
+  font-size: ${(props) => (props.isVisible ? '4rem' : '8rem')};
+  font-weight: ${(props) => (props.isVisible ? 100 : 400)};
+  color: ${(props) =>
+    props.isVisible ? 'white' : props.theme.palette.yellowBrackets};
+  text-shadow: 1px 1px 2px #000;
+  opacity: ${(props) => (props.isVisible ? 0.65 : 1)};
+`
+
+const App = styled(HeaderText)`
   font-weight: 100;
-  margin: 0;
-  letter-spacing: 1rem;
-  font-family: 'League Spartan';
+  line-height: 95%;
+  font-size: ${(props) => (props.isVisible ? '0px' : '4rem')};
+  opacity: ${(props) => (props.isVisible ? 0 : 1)};
+  position: ${(props) => (props.isVisible ? 'absolute' : 'relative')};
 `
 
-const There = styled(HeaderText)`
-  font-family: 'League Spartan';
-  font-size: 6rem;
-  font-weight: 200;
-  text-transform: uppercase;
-  margin: 0;
+const IAm = styled(HeaderText)`
+  opacity: ${(props) => (props.isVisible ? 1 : 0)};
+  width: ${(props) => (props.isVisible ? 'unset' : '0px')};
 `
 
 const Name = styled(HeaderText)`
   font-size: 8rem;
-  margin: 0;
-  text-transform: uppercase;
   line-height: 95%;
+  color: ${(props) =>
+    props.isVisible ? 'white' : props.theme.palette.darkOrange};
+  font-size: ${(props) => (props.isVisible ? '8rem' : '0px')};
+  opacity: ${(props) => (props.isVisible ? 1 : 0)};
+`
+
+const NotTypical = styled(HeaderText)`
+  font-weight: 100;
+  line-height: 95%;
+  font-size: ${(props) => (props.isVisible ? '0px' : '4rem')};
+  opacity: ${(props) => (props.isVisible ? 0 : 1)};
 `
 
 export const Intro = () => {
+  const visibility = useStore((state) => state.visibility)
+
+  const isVisible = visibility[IDENTITY.EAT]
+
   return (
     <Container>
-      <NameContainer>
-        <Hello>Hello</Hello>
+      <NameContainer isVisible={isVisible}>
         <div style={{ marginLeft: '0.5rem' }}>
+          <NotTypical isVisible={isVisible}>Not</NotTypical>
+          <NotTypical isVisible={isVisible}>Your typical</NotTypical>
+          <HelloThere isVisible={isVisible}>Hello</HelloThere>
           <div
             style={{
               display: 'flex',
               alignItems: 'center',
             }}
           >
-            <There>there</There>
-            <HeaderText
+            <HelloThere isVisible={isVisible}>
+              {isVisible ? 'there' : 'world'}
+            </HelloThere>
+            <App isVisible={isVisible}>App</App>
+            <IAm
               style={{
                 marginLeft: '1rem',
                 fontSize: '2rem',
               }}
+              isVisible={isVisible}
             >
               I&apos;m
-            </HeaderText>
+            </IAm>
           </div>
-          <Name>Peter</Name>
-          <Name>Kwan</Name>
+
+          <Name isVisible={isVisible}>Peter</Name>
+          <Name isVisible={isVisible}>Kwan</Name>
         </div>
       </NameContainer>
       <ScrollPrompt />
