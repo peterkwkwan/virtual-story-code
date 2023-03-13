68af834bf0ad29a9178be6695156d91266a224d7
peterkwkwan <peterkwkwan@gmail.com>
2023-03-13 19:38:28 +0800

    feat: world planet to have circle

--- src/pages/Explorer/Home/ParallaxLayers/Intro/WorldPlanet.tsx
+++ src/pages/Explorer/Home/ParallaxLayers/Intro/WorldPlanet.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import styled from 'styled-components'
 import { ParallaxLayer } from '@react-spring/parallax'
 
-export const WorldPlanet = styled(ParallaxLayer)<{ eatIsVisible: boolean }>`
+export const WorldPlanet = styled(ParallaxLayer)<{ show: boolean }>`
   color: white;
   z-index: 2;
   background-color: ${(props) =>
-    props.eatIsVisible ? 'transparent' : props.theme.palette.darkBlueText};
-  clip-path: ${(props) =>
-    props.eatIsVisible ? 'circle(100%)' : 'circle(10%)'};
+    props.show
+      ? props.theme.palette.persianGreen
+      : props.theme.palette.darkBlueText};
+  clip-path: ${(props) => (props.show ? 'circle(35%)' : 'circle(10%)')};
   transition: clip-path 1s, background-color 2s;
 `
