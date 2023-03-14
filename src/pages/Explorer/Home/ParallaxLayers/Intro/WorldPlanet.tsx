ed1d37c9f70c25e809d910945dace5d30d56d3ce
peterkwkwan <peterkwkwan@gmail.com>
2023-03-14 08:19:04 +0800

    feat: update WorldPlanet

--- src/pages/Explorer/Home/ParallaxLayers/Intro/WorldPlanet.tsx
+++ src/pages/Explorer/Home/ParallaxLayers/Intro/WorldPlanet.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import styled from 'styled-components'
 import { ParallaxLayer } from '@react-spring/parallax'
 
-export const WorldPlanet = styled(ParallaxLayer)<{ show: boolean }>`
+export const WorldPlanet = styled(ParallaxLayer)<{ expand: boolean }>`
   color: white;
-  z-index: 2;
   background-color: ${(props) =>
-    props.show
+    props.expand
       ? props.theme.palette.persianGreen
       : props.theme.palette.darkBlueText};
-  clip-path: ${(props) => (props.show ? 'circle(35%)' : 'circle(10%)')};
+  clip-path: ${(props) => (props.expand ? 'circle(35%)' : 'circle(10%)')};
   transition: clip-path 1s, background-color 2s;
 `
