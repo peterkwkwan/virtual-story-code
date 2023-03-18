15ec3ee10b2af6493793311d4d87129b82df2faf
peterkwkwan <peterkwkwan@gmail.com>
2023-03-18 19:16:02 +0800

    feat: formatting

--- src/pages/Explorer/Readme.tsx
+++ src/pages/Explorer/Readme.tsx
@@ -3,7 +3,12 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 import { ExplorerWrapper } from './shared/ExplorerWrapper'
-import { BaseText, DarkBlueText, LineBreak, StringText } from './shared/StyledText'
+import {
+  BaseText,
+  DarkBlueText,
+  LineBreak,
+  StringText,
+} from './shared/StyledText'
 
 import { useLastContributed } from '@/hooks/useLastContributed'
 
@@ -19,21 +24,18 @@ const ExternalLink = styled.a`
 const IntroLines = [
   'this is my virtual story',
   'journey started 2017 December',
-  'Project tech stack:'
+  'Project tech stack:',
 ]
 
-const RenderIntroLines = () => (
-  IntroLines.map(line => {
+const RenderIntroLines = () =>
+  IntroLines.map((line) => {
     return (
-      <>
-        <BlockBaseText>
-          {line}
-        </BlockBaseText>
-        <LineBreak/>
-      </>
+      <React.Fragment key={line}>
+        <BlockBaseText>{line}</BlockBaseText>
+        <LineBreak />
+      </React.Fragment>
     )
   })
-)
 
 export const Readme = () => {
   const date = new Date('2019-05-01')
@@ -45,19 +47,50 @@ export const Readme = () => {
   return (
     <ExplorerWrapper contributors={contributors} numberOfLines={15}>
       <DarkBlueText>## virtual story code</DarkBlueText>
-      <LineBreak/>
+      <LineBreak />
       {RenderIntroLines()}
       <BlockBaseText>
-        <DarkBlueText>1. </DarkBlueText>[<StringText>Vite</StringText>](<ExternalLink target="_blank" rel="noopener" href='https://vitejs.dev/'>https://vitejs.dev/</ExternalLink>) - for Über fast local hosting & bundling speeds
+        <DarkBlueText>1. </DarkBlueText>[<StringText>Vite</StringText>](
+        <ExternalLink target="_blank" rel="noopener" href="https://vitejs.dev/">
+          https://vitejs.dev/
+        </ExternalLink>
+        ) - for Über fast local hosting & bundling speeds
       </BlockBaseText>
       <BlockBaseText>
-        <DarkBlueText>2. </DarkBlueText>[<StringText>styled-components</StringText>](<ExternalLink target="_blank" rel="noopener" href="https://styled-components.com/">https://styled-components.com/</ExternalLink>) - CSS in JS, global theming, and conditional styling ;&#41;
+        <DarkBlueText>2. </DarkBlueText>[
+        <StringText>styled-components</StringText>](
+        <ExternalLink
+          target="_blank"
+          rel="noopener"
+          href="https://styled-components.com/"
+        >
+          https://styled-components.com/
+        </ExternalLink>
+        ) - CSS in JS, global theming, and conditional styling ;&#41;
       </BlockBaseText>
       <BlockBaseText>
-        <DarkBlueText>3. </DarkBlueText>[<StringText>React Router</StringText>](<ExternalLink target="_blank" rel="noopener" href="https://reactrouter.com/" >https://reactrouter.com/</ExternalLink>) - native-like site navigation
+        <DarkBlueText>3. </DarkBlueText>[<StringText>React Router</StringText>](
+        <ExternalLink
+          target="_blank"
+          rel="noopener"
+          href="https://reactrouter.com/"
+        >
+          https://reactrouter.com/
+        </ExternalLink>
+        ) - native-like site navigation
       </BlockBaseText>
-      <LineBreak/>
-      <BlockBaseText>copyright &copy; {currentYear} [peterkwkwan](<ExternalLink target="_blank" rel="noopener" href="https://github.com/peterkwkwan" >https://github.com/peterkwkwan</ExternalLink>)</BlockBaseText>
+      <LineBreak />
+      <BlockBaseText>
+        copyright &copy; {currentYear} [peterkwkwan](
+        <ExternalLink
+          target="_blank"
+          rel="noopener"
+          href="https://github.com/peterkwkwan"
+        >
+          https://github.com/peterkwkwan
+        </ExternalLink>
+        )
+      </BlockBaseText>
     </ExplorerWrapper>
   )
 }
