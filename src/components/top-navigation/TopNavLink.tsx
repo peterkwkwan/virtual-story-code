45e5534114079f8445f0c2825697d89ded15e8c9
peterkwkwan <peterkwkwan@gmail.com>
2023-03-18 19:15:32 +0800

    feat: fix console errors

--- src/components/top-navigation/TopNavLink.tsx
+++ src/components/top-navigation/TopNavLink.tsx
@@ -8,27 +8,31 @@ interface Props {
   gradient: boolean
 }
 
-const StyledLink = styled(Link)<{ gradient?: boolean }>`
-    padding: 0 12px;
-    border-radius: 5px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-family: SF-Pro, sans-serif;
-    text-decoration: none;
-    height: 100%;
-    color: ${(props) =>
-    props.gradient ? props.theme.palette.dark01 : props.theme.palette.white};
-    :nth-of-type(1) {
-      font-weight: 600;
-    }
-    &:hover {
-      background-color: rgba(0,0,0,0.1);
-    }
+const StyledLink = styled(Link)<{ gradient: string }>`
+  padding: 0 12px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: SF-Pro, sans-serif;
+  text-decoration: none;
+  height: 100%;
+  color: ${(props) =>
+    props.gradient === 'true'
+      ? props.theme.palette.dark01
+      : props.theme.palette.white};
+  :nth-of-type(1) {
+    font-weight: 600;
+  }
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.1);
+  }
 `
 
-export const TopNavLink = ({path, name, gradient} : Props) => {
+export const TopNavLink = ({ path, name, gradient }: Props) => {
   return (
-    <StyledLink to={path} gradient={gradient}>{name}</StyledLink>
+    <StyledLink to={path} gradient={gradient ? 'true' : 'false'}>
+      {name}
+    </StyledLink>
   )
 }
