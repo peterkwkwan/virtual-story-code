3ecdda06cb029747a2363664c8ae18b7007bc92d
peterkwkwan <peterkwkwan@gmail.com>
2023-03-18 19:16:59 +0800

    feat: adjust planetLayer size

--- src/pages/Explorer/Home/ParallaxLayers/Intro/PlanetLayer.tsx
+++ src/pages/Explorer/Home/ParallaxLayers/Intro/PlanetLayer.tsx
@@ -2,37 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 import { ParallaxLayerProps, ParallaxLayer } from '@react-spring/parallax'
 
-interface PlanetProps {
-  small?: boolean
-  medium?: boolean
-  large?: boolean
-}
-
-type Props = ParallaxLayerProps & PlanetProps
-
-const Planet = styled.img<PlanetProps>`
-  width: 20%;
-  width: ${(props) => props.small && '30%'};
-  width: ${(props) => props.medium && '40%'};
-  width: ${(props) => props.large && '50%'};
+const Planet = styled.img`
+  width: 50%;
   filter: brightness(40%);
   transition: width 2s;
 `
 
-export const PlanetLayer = (props: Props) => {
-  const { small, medium, large, ...rest } = props
-  const planetProps = { small, medium, large }
-
+export const PlanetLayer = (props: ParallaxLayerProps) => {
   return (
     <ParallaxLayer
-      {...rest}
+      {...props}
       style={{
         display: 'flex',
         justifyContent: 'start',
         alignItems: 'end',
       }}
     >
-      <Planet {...planetProps} src="/assets/images/home/planet.png" />
+      <Planet src="/assets/images/home/planet.png" />
     </ParallaxLayer>
   )
 }
