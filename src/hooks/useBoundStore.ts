fbc77e46f353a94f6147745ff23f947b18ac6bd0
peterkwkwan <peterkwkwan@gmail.com>
2023-03-20 12:25:59 +0800

    feat: update useBoundStore

--- src/hooks/useBoundStore.ts
+++ src/hooks/useBoundStore.ts
@@ -5,13 +5,18 @@ import {
   OffsetPercentageSlice,
 } from './slices/createOffsetPercentageSlice'
 import {
+  createSpeechTrackerSlice,
+  SpeechTrackerSlice,
+} from './slices/createSpeechTrackerSlice'
+import {
   createVisibilitySlice,
   VisibilitySlice,
 } from './slices/createVisibilitySlice'
 
-export const useBoundStore = create<VisibilitySlice & OffsetPercentageSlice>()(
-  (...a) => ({
-    ...createVisibilitySlice(...a),
-    ...createOffsetPercentageSlice(...a),
-  })
-)
+type Slices = OffsetPercentageSlice & SpeechTrackerSlice & VisibilitySlice
+
+export const useBoundStore = create<Slices>()((...a) => ({
+  ...createVisibilitySlice(...a),
+  ...createOffsetPercentageSlice(...a),
+  ...createSpeechTrackerSlice(...a),
+}))
