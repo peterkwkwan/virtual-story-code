39f082830a659836a94a5d67e183fb38ffddc26c
peterkwkwan <peterkwkwan@gmail.com>
2023-03-21 11:12:08 +0800

    feat: searchContainer formatting

--- src/pages/Search/SearchContainer.tsx
+++ src/pages/Search/SearchContainer.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import styled from "styled-components";
-import { ReplaceField } from "./ReplaceField";
-import { SearchField } from "./SearchField";
-import { SearchResult } from "./SearchResult";
+import React from 'react'
+import styled from 'styled-components'
+
+import { ReplaceField } from './ReplaceField'
+import { SearchField } from './SearchField'
+import { SearchResult } from './SearchResult'
 
 const Container = styled.div`
   font-family: SF-Pro, sans-serif;
-`;
+`
 
 export const SearchContainer = () => {
   return (
@@ -15,5 +16,5 @@ export const SearchContainer = () => {
       <ReplaceField />
       <SearchResult />
     </Container>
-  );
-};
+  )
+}
