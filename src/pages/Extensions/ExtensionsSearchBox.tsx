f8653d50483edc8feaaffb7e615989fdb02250fb
peterkwkwan <peterkwkwan@gmail.com>
2023-03-21 09:13:18 +0800

    feat: search box cleanup

--- src/pages/Extensions/ExtensionsSearchBox.tsx
+++ src/pages/Extensions/ExtensionsSearchBox.tsx
@@ -1,21 +1,11 @@
 import styled from 'styled-components'
 
-export const TextField = styled.input`
-  color: ${(props) => props.theme.palette.text01};
-  background-color: ${(props) => props.theme.palette.dark04};
-  height: 24px;
-  width: calc(100% - 18px - 20px);
-  margin: 0 12px 0 20px;
-  outline: none;
-  border: 1px solid transparent;
-  ::placeholder {
-    color: ${(props) => props.theme.palette.text03};
-  }
-  :focus {
-    border: ${(props) => '1px solid ' + props.theme.palette.selectedBlueBorder};
-  }
-`
+import { TextInput } from '@/components/elements/TextInput'
 
 export const ExtensionsSearchBox = () => {
-  return <TextField placeholder="Search Peter's skills" />
+  return (
+    <form spellCheck="false">
+      <TextInput placeholder="Search Peter's skills" />
+    </form>
+  )
 }
