7b4d34cdbdf9b21ea87fd85f4c59a083e509dae9
peterkwkwan <peterkwkwan@gmail.com>
2023-03-31 01:17:52 +0800

    feat: add routerConfigs

--- src/pages/shared/routerConfig.ts
+++ src/pages/shared/routerConfig.ts
@@ -15,6 +15,18 @@ import { BCWGroup } from '../explorer/BCWGroup'
 import { Readme } from '../explorer/Readme'
 import { Home } from '../explorer/Home'
 import { ExtReact } from '../extensions/ExtReact'
+import { ExtReactRouter } from '../extensions/ExtReactRouter'
+import { ExtReactQuery } from '../extensions/ExtReactQuery'
+import { ExtCss } from '../extensions/ExtCss'
+import { ExtHtml } from '../extensions/ExtHtml'
+import { ExtJavascript } from '../extensions/ExtJavascript'
+import { ExtTailwind } from '../extensions/ExtTailwind'
+import { ExtTypescript } from '../extensions/ExtTypescript'
+import { ExtStyledComponents } from '../extensions/ExtStyledComponents'
+import { ExtMui } from '../extensions/ExtMui'
+import { ExtStorybook } from '../extensions/ExtStorybook'
+import { ExtStripe } from '../extensions/ExtStripe'
+import { ExtGit } from '../extensions/ExtGit'
 
 export enum PagePaths {
   HOME = 'explorer',
@@ -33,7 +45,19 @@ export enum PagePaths {
   README = 'explorer/readme',
   SEARCH = 'search',
   EXTENSIONS = 'extensions',
-  REACT = 'extensions/react',
+  EXT_REACT = 'extensions/react',
+  EXT_REACTROUTER = 'extensions/react-router',
+  EXT_REACTQUERY = 'extensions/react-query',
+  EXT_JAVASCRIPT = 'extensions/javascript',
+  EXT_TYPESCRIPT = 'extensions/typescript',
+  EXT_HTML = 'extensions/html',
+  EXT_CSS = 'extensions/css',
+  EXT_STYLEDCOMPONENTS = 'extensions/styled-components',
+  EXT_TAILWIND = 'extensions/tailwind',
+  EXT_MUI = 'extensions/mui',
+  EXT_STORYBOOK = 'extensions/storybook',
+  EXT_STRIPE = 'extensions/stripe',
+  EXT_GIT = 'extensions/git',
 }
 
 export enum PageNames {
@@ -50,7 +74,19 @@ export enum PageNames {
   README = 'README.md',
   PACKAGE_JSON = 'package.json',
   GIT_IGNORE = '.gitignore',
-  REACT = 'Skill: React',
+  EXT_REACT = 'Skill: React',
+  EXT_REACTROUTER = 'Skill: React Router',
+  EXT_REACTQUERY = 'Skill: React Query',
+  EXT_JAVASCRIPT = 'Skill: JavaScript',
+  EXT_TYPESCRIPT = 'Skill: TypeScript',
+  EXT_HTML = 'Skill: HTML',
+  EXT_CSS = 'Skill: CSS',
+  EXT_STYLEDCOMPONENTS = 'Skill: styled-components',
+  EXT_TAILWIND = 'Skill: Tailwind',
+  EXT_MUI = 'Skill: MUI',
+  EXT_STORYBOOK = 'Skill: Storybook',
+  EXT_STRIPE = 'Skill: Stripe',
+  EXT_GIT = 'Skill: Git',
 }
 
 interface RouterConfig {
@@ -137,8 +173,68 @@ export const routerConfig: RouterConfig[] = [
     component: ExtensionsContent,
   },
   {
-    path: PagePaths.REACT,
+    path: PagePaths.EXT_REACT,
     component: ExtReact,
-    fileName: PageNames.REACT,
+    fileName: PageNames.EXT_REACT,
+  },
+  {
+    path: PagePaths.EXT_REACTROUTER,
+    component: ExtReactRouter,
+    fileName: PageNames.EXT_REACTROUTER,
+  },
+  {
+    path: PagePaths.EXT_REACTQUERY,
+    component: ExtReactQuery,
+    fileName: PageNames.EXT_REACTQUERY,
+  },
+  {
+    path: PagePaths.EXT_CSS,
+    component: ExtCss,
+    fileName: PageNames.EXT_CSS,
+  },
+  {
+    path: PagePaths.EXT_HTML,
+    component: ExtHtml,
+    fileName: PageNames.EXT_HTML,
+  },
+  {
+    path: PagePaths.EXT_JAVASCRIPT,
+    component: ExtJavascript,
+    fileName: PageNames.EXT_JAVASCRIPT,
+  },
+  {
+    path: PagePaths.EXT_TYPESCRIPT,
+    component: ExtTypescript,
+    fileName: PageNames.EXT_TYPESCRIPT,
+  },
+  {
+    path: PagePaths.EXT_STYLEDCOMPONENTS,
+    component: ExtStyledComponents,
+    fileName: PageNames.EXT_STYLEDCOMPONENTS,
+  },
+  {
+    path: PagePaths.EXT_TAILWIND,
+    component: ExtTailwind,
+    fileName: PageNames.EXT_TAILWIND,
+  },
+  {
+    path: PagePaths.EXT_MUI,
+    component: ExtMui,
+    fileName: PageNames.EXT_MUI,
+  },
+  {
+    path: PagePaths.EXT_STORYBOOK,
+    component: ExtStorybook,
+    fileName: PageNames.EXT_STORYBOOK,
+  },
+  {
+    path: PagePaths.EXT_STRIPE,
+    component: ExtStripe,
+    fileName: PageNames.EXT_STRIPE,
+  },
+  {
+    path: PagePaths.EXT_GIT,
+    component: ExtGit,
+    fileName: PageNames.EXT_GIT,
   },
 ]
