2c639acd72e4967dc892fc5c467256768f7a1808
peterkwkwan <peterkwkwan@gmail.com>
2023-04-01 14:39:04 +0800

    feat: add company logos

--- src/pages/Explorer/shared/types.d.ts
+++ src/pages/Explorer/shared/types.d.ts
@@ -1,31 +1,33 @@
+import { Companies } from '@/constants/companies'
+
 export interface IJobDescription {
-  role: string;
-  isCurrentRole?: boolean;
-  companyName: string;
-  date: string;
-  location: string;
-  companyDescription: string;
-  functions: string[];
-  techStack: string[];
+  role: string
+  isCurrentRole?: boolean
+  companyName: Companies
+  date: string
+  location: string
+  companyDescription: string
+  functions: string[]
+  techStack: string[]
 }
 
-export type INonTechJobDescription = Omit<IJobDescription, 'techStack'>;
+export type INonTechJobDescription = Omit<IJobDescription, 'techStack'>
 
 export interface IEducationDescription {
-  schoolName: string;
-  year: string;
-  location: { city: string; country: string };
-  degreeType: string;
-  degreeName: string;
-  subjects?: string[];
+  schoolName: string
+  year: string
+  location: { city: string; country: string }
+  degreeType: string
+  degreeName: string
+  subjects?: string[]
 }
 
 interface File {
-  title: string;
-  path: string;
+  title: string
+  path: string
 }
 
 export interface FolderContent {
-  opened: boolean;
-  files?: File[];
+  opened: boolean
+  files?: File[]
 }
