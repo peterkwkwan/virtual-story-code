18022bdfaf2eb55da4e6b25f0114534d181cbda9
peterkwkwan <peterkwkwan@gmail.com>
2023-04-08 17:51:09 +0800

    feat: blog home

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
+import { BaseContentContainer } from '../shared/styledContainers'
+
+import { LetterPressHome } from '@/components/elements/LetterPressHome'
+
+const TIPS = [
+  {
+    text: 'Click on any article in the sidebar',
+    src: '/assets/icons/arrow-left.png',
+  },
+]
+
 export const Blog = () => {
-  return <div>Blog</div>
+  return (
+    <BaseContentContainer>
+      <LetterPressHome tips={TIPS} />
+    </BaseContentContainer>
+  )
 }
