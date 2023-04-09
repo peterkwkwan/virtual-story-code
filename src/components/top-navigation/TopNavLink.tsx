42b2a5c40ec7acea0a73bb9fee9d082ea5d5d9cd
peterkwkwan <peterkwkwan@gmail.com>
2023-04-09 21:53:40 +0800

    feat: add activePath styling to top nav

--- src/components/top-navigation/TopNavLink.tsx
+++ src/components/top-navigation/TopNavLink.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
+import { useActivePath } from '@/hooks/useActivePath'
+
 interface Props {
   path: string
   name: string
   gradient: boolean
 }
 
-const StyledLink = styled(Link)<{ gradient: string }>`
+const StyledLink = styled(Link)<{ gradient: string; active: boolean }>`
   padding: 0 12px;
   border-radius: 5px;
   display: flex;
@@ -21,6 +23,12 @@ const StyledLink = styled(Link)<{ gradient: string }>`
     props.gradient === 'true'
       ? props.theme.palette.dark01
       : props.theme.palette.white};
+  background-color: ${(props) =>
+    props.active
+      ? props.gradient === 'true'
+        ? 'rgba(0, 0, 0, 0.1)'
+        : 'rgba(255, 255, 255, 0.25)'
+      : ''};
   :nth-of-type(1) {
     font-weight: 600;
   }
@@ -30,8 +38,13 @@ const StyledLink = styled(Link)<{ gradient: string }>`
 `
 
 export const TopNavLink = ({ path, name, gradient }: Props) => {
+  const isActive = useActivePath(path)
   return (
-    <StyledLink to={path} gradient={gradient ? 'true' : 'false'}>
+    <StyledLink
+      to={path}
+      gradient={gradient ? 'true' : 'false'}
+      active={isActive}
+    >
       {name}
     </StyledLink>
   )
