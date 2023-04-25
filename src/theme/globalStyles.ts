b17d754719b2514139671f18314adc948bb3c47f
peterkwkwan <peterkwkwan@gmail.com>
2023-04-26 00:47:55 +0800

    feat: remove menlo font

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle<{ isBlackNWhite: boolean }>`
   body {
-    font-family: Menlo, Monaco, "Courier New", monospace;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     padding: 0;
     margin: 0;
   }
