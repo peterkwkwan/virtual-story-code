0d59f30d09c2d977ae0f0a6f9ae71b01c8d25702
peterkwkwan <peterkwkwan@gmail.com>
2023-04-26 22:54:38 +0800

    feat: jobs and education map strings

--- src/pages/Explorer/shared/editorUtils.ts
+++ src/pages/Explorer/shared/editorUtils.ts
@@ -16,20 +16,14 @@ export const WillisTowersWatson = () => {
   job.location = "${job.location}"
   job.companyDescription = "${job.companyDescription}"
   
-  job.function1 = {
-    task: "Migrated Material UI v4 to v5 in Career Navigator legacy app, refactoring over 3000 lines of code with respect to breaking changes. Resulted in improved developer experience and resolved 20+ legacy bugs"
+  job.function = {${job.functions.map((task, index) => {
+    return `\n    task${index + 1}: "${task}"`
+  })}
   }
   
-  const techStack = [
-    "React",
-    "Gatsby",
-    "TypeScript",
-    "JavaScript",
-    "C#",
-    "AngularJS",
-    "styled-components",
-    "Material UI",
-    "Git",
+  const techStack = [${job.techStack.map((stack) => {
+    return `\n    "${stack}"`
+  })}
   ]
   
   job.integrate(techStack)
@@ -46,18 +40,13 @@ export const getScssEditorDefaultValue = (job: INonTechJobDescription) => {
     grid-area: "${job.location}";
     background: "${job.companyDescription}";
 
-    >ul {
-        li:nth-child(1) {
-            content: "Official training mentor for 3 new joiners in the Relationship Management team.";
-        }
-
-        li:nth-child(2) {
-            content: "Managed 50+ client relationships across investment banking, law firm & private equity sectors to ensure the client’s needs are met.";
-        }
-
-        li:nth-child(3) {
-            content: "Identify client motivations, enabling cross-selling plus up-selling opportunities.";
-        }
+    >ul {${job.functions
+      .map((task, index) => {
+        return `\n        li:nth-child(${index + 1}) {
+            content: "${task}";
+        }\n\n`
+      })
+      .join('')}
     }
 }`
 }
@@ -74,12 +63,20 @@ export const getHtmlEditorDefaultValue = (
   <meta name="degree" content="${educationDescription.degreeType}">
   <meta name="year" content="${educationDescription.year}">
   <title>${educationDescription.schoolName}</title>
-  <link rel="${educationDescription.location.city}" href="${educationDescription.location.country}">
+  <link rel="${educationDescription.location.city}" href="${
+    educationDescription.location.country
+  }">
 </head>
 
 <body>
-  <h1>Bachelor of Arts</h1>
-  <h2>Major in Economics</h2>
-  <h2>Minor in International Development</h2>
+  <h1>${educationDescription.degreeName}</h1>${
+    educationDescription.subjects
+      ? educationDescription.subjects
+          ?.map((subject) => {
+            return `\n  <h2>${subject}</h2>`
+          })
+          .join('')
+      : ''
+  }
 </body>`
 }
